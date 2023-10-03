d11bd89e9f392d6fa50808f7e4c5395d1f8a8595
Student <Student@205-64>
2023-10-03 15:46:18 +0800

    加入專案檔案。

---
+++ FruitApp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace FruitApp
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Fruit> list = new List<Fruit>()
+        {
+            new Fruit(){Name="Apple",Image="apple.png",Description="This is an apple"},
+            new Fruit(){Name="Orange",Image="orange.png",Description="This is an orange"},
+            new Fruit(){Name="Banana",Image="banana.png",Description="This is a banana"},
+            new Fruit(){Name="Kiwi",Image="kiwi.png",Description="This is a kiwi"},
+            new Fruit(){Name="Strawberry",Image="strawberry.png",Description="This is a strawberry"},
+            new Fruit(){Name="Pineapple",Image="pineapple.png",Description="This is a pineapple"},
+            new Fruit(){Name="Watermelon",Image="watermelon.png",Description="This is a watermelon"},
+            new Fruit(){Name="Grapes",Image="grape.png",Description="This is a grapes"},
+        };
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            FruitListView.ItemsSource = list;
+
+        }
+        private void FruitListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            var selectedFruit = e.SelectedItem as Fruit;
+            Navigation.PushAsync(new FruitDetailed(selectedFruit.Name, selectedFruit.Image, selectedFruit.Description));
+        }
+
+
+    }
+}
